316d6e14e8abe7e30a02516f3067a637eb714173
Steven Bister <stevenbister@macbook-pro.home>
2020-09-13 17:09:14 +0100

    Added: Custom fonts and Favicon icon

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -27,6 +27,29 @@ module.exports = {
           include: /\.inline\.svg$/
         }
       }
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: 'Open Sans',
+            variants: ['400', '400i', '600', '600i']
+          }
+        ],
+        display: 'swap'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Cakes with Grace',
+        short_name: 'Cakes with Grace',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#4C4A4F',
+        icon: 'src/images/Favicon.png'
+      }
     }
   ]
 }
